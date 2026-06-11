bb5827b9d7d6398aebfa7a22583c03cd75be5709
STharshan <sivatharshan81@gmail.com>
2026-06-11 19:22:26 +0530

    add change

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Ansely from "./pages/Ansely";
 import Damien from "./pages/Daiman";
 import Josh from "./pages/Josh";
+import MusicHubPage from "./pages/MusicHubPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/g.selva" element={<Ansely/>} />
         <Route path="/damien" element={<Damien />} />
         <Route path="/josh" element={<Josh />} />
+        <Route path="/music-hub" element={<MusicHubPage />} />
       </Routes>
     </Router>
   );
